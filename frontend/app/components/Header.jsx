1cc334e3e00510557eb4998b61ce537c11e49caa
Hayley Smith <126207971+hayley9080@users.noreply.github.com>
2024-08-22 15:45:16 +1200

    fix: link issue with <a> (#50)

--- frontend/app/components/Header.jsx
+++ frontend/app/components/Header.jsx
@@ -6,10 +6,13 @@ import { Button } from "@nextui-org/react";
 export default function Header() {
   return (
     <div className="bg-white py-4 px-6 flex justify-between items-center">
-      <Link href="/" passHref>
-        <a className="text-4xl font-bold text-purple-700 cursor-pointer">
+      <Link
+        href="/"
+        passHref
+        className="text-4xl font-bold text-purple-700 cursor-pointer">
+        
           MEALMATCH
-        </a>
+        
       </Link>
       <div className="flex space-x-4">
         <Button color="primary" variant="ghost" size="lg">
